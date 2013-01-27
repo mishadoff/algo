fbd709bf42bc4707c5cab1d18505005f8bb1bea5
mishadoff <mishadoff@gmail.com>
2013-01-27 17:45:13 +0200

    SortUtils class added

---
+++ src/com/mishadoff/algo/sorting/SortUtils.java
@@ -0,0 +1,30 @@
+package com.mishadoff.algo.sorting;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Class consists of different helper methods for sorting.
+ * 
+ * @author mishadoff
+ *
+ */
+public final class SortUtils {
+	
+	private SortUtils() { }
+
+	// TODO add tests
+	public static <T> boolean isSorted(List<T> elem, Comparator<T> comp) {
+		boolean sorted = true;
+		for (int i = 0; i < elem.size() - 1; i++) {
+			T e1 = elem.get(i);
+			T e2 = elem.get(i + 1);
+			if (comp.compare(e1, e2) > 0) {
+				sorted = false;
+				break;
+			}
+		}
+		return sorted;
+	}
+	
+}

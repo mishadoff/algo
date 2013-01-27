208a888c3ebc30adfa685e173a2d0b8acc9733fa
mishadoff <mishadoff@gmail.com>
2013-01-27 20:35:04 +0200

    Merge Sort stub added

---
+++ src/com/mishadoff/algo/sorting/MergeSort.java
@@ -0,0 +1,20 @@
+package com.mishadoff.algo.sorting;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MergeSort implements Sort {
+	
+	@Override
+	public <T> void sort(List<T> elem, Comparator<T> comp) {
+		// divide array
+		// merge sort each of them
+		// merge results
+		
+	}
+	
+	private <T> void merge(List<T> elem) {
+		
+	}
+
+}

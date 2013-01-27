d5631bb4fd937f480e51a85cfa873c7333b46009
mishadoff <mishadoff@gmail.com>
2013-01-27 20:34:40 +0200

    Binary Search stub added

---
+++ src/com/mishadoff/algo/search/BinarySearch.java
@@ -0,0 +1,14 @@
+package com.mishadoff.algo.search;
+
+import java.util.List;
+
+public class BinarySearch implements Search {
+	
+	/**
+	 * Input list nust be sorted
+	 */
+	@Override
+	public <T> int search(List<T> elem, T e) {
+		return -1;
+	}
+}

c0f00efece20a669e06b1fa57d8beecdf2f0f11f
mishadoff <mishadoff@gmail.com>
2013-02-08 19:13:44 +0200

    First implementation of QuickUnion algorithm

---
+++ src/com/mishadoff/algo/connectivity/QuickUnion.java
@@ -0,0 +1,62 @@
+package com.mishadoff.algo.connectivity;
+
+/**
+ * Improved algorithm for connectivity problem.
+ * 
+ * Compexity:
+ * union O(n)
+ * isConnected O(n)
+ * 
+ * @author mishadoff
+ *
+ */
+public class QuickUnion implements Connectivity {
+    
+    private int[] connections;
+    
+    public QuickUnion(int N) {
+        connections = new int[N];
+        for (int i = 0; i < N; i++) {
+            connections[i] = i;
+        }
+    }
+    
+    @Override
+    public void union(int i, int j) {
+        int first = findRoot(i);
+        int second = findRoot(j);
+        connections[first] = second;
+    }
+    
+    @Override
+    public boolean isConnected(int i, int j) {
+        return findRoot(i) == findRoot(j);
+    }
+    
+    /**
+     * Return root component id for passed one.
+     * @param i
+     * @return
+     */
+    private int findRoot(int i) {
+        while (i != connections[i]) {
+            i = connections[i];
+        }
+        return i;
+    }
+
+    
+    // --------------------- TEST
+
+    public static void main(String[] args) {
+        Connectivity con = new QuickUnion(10);
+        //6-3 3-7 4-0 3-8 9-6 5-0
+            con.union(6, 3);
+            con.union(3, 7);
+            con.union(4, 0);
+            con.union(3, 8);
+            con.union(9, 6);
+            con.union(5, 0);
+        System.out.println(con.isConnected(0, 3));
+    }
+}
